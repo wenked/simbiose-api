a70269aa2705c0ddb6329fc6fc886a1156bed67d
wenked <wenked.matheus@yahoo.com.br>
2022-05-14 00:29:37 -0300

    ajustando resposta da api e tratamento de erros

--- app/Http/Controllers/RouanetController.php
+++ app/Http/Controllers/RouanetController.php
@@ -6,18 +6,36 @@ use Illuminate\Http\Request;
 use App\Models\ProjetoRouanet;
 
 
+
+
 class RouanetController extends Controller
 {
     //
  
     public function index(Request $request)
-    {
-        $page = $request->query('page');
-        $limit = $request->query('limit');
-        $offset = ($limit * $page) - $limit;
+    {   
+      
+        try {
+          
+            $page = $request->query('page');
+            $limit = $request->query('limit');
+    
+            if(!$page && !$limit) {
+                $projetos = ProjetoRouanet::all();
+                return response()->json(['data' => $projetos]);
+            }
+            
+            $offset = ($limit * $page) - $limit;
+    
+            $projetos = ProjetoRouanet::offset($offset)->limit($limit)->get();
 
-        $projetos = ProjetoRouanet::offset($offset)->limit($limit)->get();
-        return response()->json($projetos);
+            $total = ProjetoRouanet::count();
+            return response()->json(['data' => $projetos, 'current_page' => (int)$page, 'limit' => (int)$limit, 'total_pages' => ceil($total / $limit)]);
+       
+        }catch(\Exception $e){
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+       
     }
     
 
